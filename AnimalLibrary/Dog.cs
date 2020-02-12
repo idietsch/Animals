2660d6bcf621033f5cef41414cdb97d634fe0532
Ian Dietsch <wolffox36007@gmail.com>
2020-02-12 10:51:35 -0500

    Set up first properties

---
+++ AnimalLibrary/Dog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AnimalLibrary {
+    public class Dog {
+        public bool Longtail { get; set; } //Long, short
+        public string BarkPitch { get; set; } //high, medium, low
+        public string MuzzleLength { get; set; } //long, short, collapsed
+        public bool ExtremeSenseOfSmell { get; set; } = false;  //Extreme, normal
+
+        public Dog() {
+
+        }
+    }
+}
