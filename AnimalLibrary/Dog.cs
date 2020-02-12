1b22725e363ad95107b0cbba312633b8fa498ec5
Ian Dietsch <wolffox36007@gmail.com>
2020-02-12 11:23:38 -0500

    Set enumerators

--- AnimalLibrary/Dog.cs
+++ AnimalLibrary/Dog.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 
 namespace AnimalLibrary {
+    
+    public enum BarkPitch {High, Medium, Low};
+    public enum MuzzleType {Long, Short, Collapsed,};
     public class Dog {
         public bool Longtail { get; set; } //Long, short
-        public string BarkPitch { get; set; } //high, medium, low
-        public string MuzzleLength { get; set; } //long, short, collapsed
+        public BarkPitch Bark { get; set; } //high, medium, low
+        public MuzzleType Muzzle { get; set; } //long, short, collapsed
         public bool ExtremeSenseOfSmell { get; set; } = false;  //Extreme, normal
 
         public Dog() {
--- Animals/Program.cs
+++ Animals/Program.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using AnimalLibrary;
 
 namespace Animals {
     class Program {
         static void Main(string[] args) {
-            
+            var germanSheperd = new Dog {
+                Longtail = true,
+                Bark = BarkPitch.Low,
+                Muzzle = MuzzleType.Long,
+                ExtremeSenseOfSmell = true
+            };
         }
     }
 }
